df197988c83debe1dd5393bda93b2c8c7ca4c2de
Marcel Kuhmann <der_marcell@hotmail.com>
2018-05-24 06:51:42 +0200

    Update update.php * Verwendung von Funktion die es vor dem Update nicht gab Version 2.0.1

--- update.php
+++ update.php
@@ -5,20 +5,29 @@
  * @var rex_addon $this
  */
 
-$clang_prefix = cookie_consent::getKeyPrefix();
+$prefix = rex_clang::getCurrent()->getCode().'_';
+if (rex_addon::exists('yrewrite') && rex_addon::get('yrewrite')->isInstalled()) {
+    rex_yrewrite::init();
+    $domain = rex_yrewrite::getCurrentDomain();
+    if (!$domain) {
+        $domain = rex_yrewrite::getDefaultDomain();
+    }
+    $prefix .= $domain->getId();
+}
+$prefix .= '_';
 
 $configs = $this->getConfig();
 foreach ($configs as $key => $value) {
-    if (strpos($key, $clang_prefix) !== 0) {
+    if (strpos($key, $prefix) !== 0) {
         // multilanguage update needed
 
         $this->removeConfig($key);
         if (strpos($key, 'cookiedingsbums_') === 0) {
             $key = str_replace('cookiedingsbums_', '', $key);
         }
-        $this->setConfig($clang_prefix.$key, $value);
+        $this->setConfig($prefix.$key, $value);
     }
 }
-if (!$this->hasConfig($clang_prefix.'status')) {
-    $this->setConfig($clang_prefix.'status', '1');
+if (!$this->hasConfig($prefix.'status')) {
+    $this->setConfig($prefix.'status', '1');
 }
