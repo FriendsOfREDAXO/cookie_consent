8abb2b97205d9538a7bb60dc790f875eb7e71959
Marcel Kuhmann <der_marcell@hotmail.com>
2018-06-20 20:19:08 +0200

    Konfigurationstest überarbeitet (#70)

--- pages/configuration_test.php
+++ pages/configuration_test.php
@@ -1,3 +1,8 @@
+<?php
+/**
+ * @var $this rex_addon
+ */
+?>
 <style>
 	
 	#cookiedingsbums_wrapper {
@@ -40,42 +45,153 @@
 
 </style>
 
+
+
+<?php
+$context = new rex_context();
+$context->setParam('page', rex_request('page', 'string', null));
+$context->setParam('clang', rex_request('clang', 'string', null));
+$context->setParam('domain', rex_request('domain', 'string', null));
+if (!$context->getParam('clang')) {
+    $context->setParam('clang', rex_clang::getCurrentId());
+}
+if ($context->getParam('domain') === null) {
+    $domainId = '';
+    if (cookie_consent::checkYrewrite()) {
+        $allDomains = rex_yrewrite::getDomains();
+        unset($allDomains['default']);
+        if (count($allDomains) > 0) {
+            $curDomain = reset($allDomains);
+            $domainId = $curDomain->getId();
+        }
+    }
+    $context->setParam('domain', $domainId);
+}
+
+$clangId = $context->getParam('clang');
+$domainId = $context->getParam('domain');
+
+$formElements = [];
+
+if (cookie_consent::checkYrewrite()) {
+    $button_label = '';
+    $items = [];
+    foreach (rex_yrewrite::getDomains() as $id => $domain) {
+        $item = [];
+        $item['title'] = $domain->getName();
+        $item['href'] = $context->getUrl(['domain' => $domain->getId()]);
+        if ($domain->getId() == $context->getParam('domain')) {
+            $item['active'] = true;
+            $button_label = $domain->getName();
+        }
+        $items[] = $item;
+    }
+    $fragment = new rex_fragment();
+    $fragment->setVar('class', 'rex-language');
+    $fragment->setVar('button_label', $button_label);
+    $fragment->setVar('header', $this->i18n('select_domain'));
+    $fragment->setVar('items', $items, false);
+
+    $formElements[] = [
+        'label' => '<label>'.$this->i18n('select_domain').'</label>',
+        'field' => $fragment->parse('core/dropdowns/dropdown.php'),
+    ];
+}
+
+if (rex_clang::count() > 1) {
+    $formElements[] = [
+        'label' => '<label>'.$this->i18n('select_language').'</label>',
+        'field' => rex_view::clangSwitchAsDropdown($context),
+    ];
+}
+
+if (count($formElements) > 0) {
+    $fragment = new rex_fragment();
+    $fragment->setVar('elements', $formElements, false);
+    $filterContent = $fragment->parse('core/form/container.php');
+
+    $fragment = new rex_fragment();
+    $fragment->setVar('title', $this->i18n('preview_for'));
+    $fragment->setVar('body', $filterContent, false);
+    echo $fragment->parse('core/page/section.php');
+}
+
+$context = rex_context::restore();
+if (!$context->getParam('clang')) {
+    $clangId = rex_clang::getCurrentId();
+} else {
+    $clangId = $context->getParam('clang');
+}
+
+$clang_prefix = rex_clang::get($clangId)->getCode().'_';
+
+if (cookie_consent::checkYrewrite()) {
+    $domain = rex_yrewrite::getDomainById($domainId);
+    if (!$domain) {
+        $domain = rex_yrewrite::getDefaultDomain();
+    }
+    $clang_prefix .= $domain->getId();
+    $domainName = $domain->getName();
+} else {
+    $domain = null;
+    $domainName = '';
+}
+$clang_prefix .= '_';
+
+if (rex_config::get('cookie_consent', cookie_consent::getKeyPrefix().'theme', '') == 'clean') {
+    $cssFile = 'css/cookie_consent_insites_clean.css';
+} else {
+    $cssFile = 'css/cookie_consent_insites.css';
+}
+$jsFile = 'js/cookie_consent_insites.js';
+$cssFileUrl = $this->getAssetsUrl($cssFile);
+$jsFileUrl = $this->getAssetsUrl($jsFile);
+?>
+    <link rel="stylesheet" href="<?php echo $cssFileUrl; ?>">
+    <script type="text/javascript" src="<?php echo $jsFileUrl; ?>"></script>
+    <script>
+      function showPreviewConsent() {
+        document.cookie = "<?php echo cookie_consent::COOKIE_NAME; ?>=;path=/;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        <?php echo cookie_consent::cookie_consent_backend(); ?>
+      }
+    </script>
 <?php
 
-$theme = rex_config::get('cookie_consent', 'theme');
-$color_background = rex_config::get('cookie_consent', 'color_background');
-$color_main_content = rex_config::get('cookie_consent', 'color_main_content');
-$color_button_background = rex_config::get('cookie_consent', 'color_button_background');
-$color_button_content = rex_config::get('cookie_consent', 'color_button_content');
-$position = rex_config::get('cookie_consent', 'position');
-$main_message = rex_config::get('cookie_consent', 'main_message');
-$button_content = rex_config::get('cookie_consent', 'button_content');
-$link_content = rex_config::get('cookie_consent', 'link_content');
-$link = rex_config::get('cookie_consent', 'iLink');
-$interner_link = '';
-if ($link != '') {
-    $interner_link = rex_getUrl($link);
-}
-$externer_link = rex_config::get('cookie_consent', 'eLink');
-$mode = rex_config::get('cookie_consent', 'mode');
-$deny_content = rex_config::get('cookie_consent', 'deny_content');
-$allow_content = rex_config::get('cookie_consent', 'allow_content');
+// ----------------- FILTER SECTION
 
-$main_color_scheme = 'style="background:'.rex_escape($color_background).'; color: '.rex_escape($color_main_content).';"';
-$link_color_scheme = 'style="color: '.rex_escape($color_main_content).';"';
-$button_color_scheme = 'style="background:'.rex_escape($color_button_background).'; color:'.rex_escape($color_button_content).';"';
+$button = [
+    [
+        'label' => $this->i18n('preview'),
+        'icon' => 'view',
+        'attributes' => [
+            'class' => ['btn-lg', 'btn-primary'],
+            'onclick' => 'showPreviewConsent()',
+        ],
+    ],
+];
+$fragment = new rex_fragment();
+$fragment->setVar('buttons', $button, false);
+$previewSection = $fragment->parse('core/buttons/button.php');
 
-echo '<h2>Cookie Consent Test</h2>';
-echo '<p>Das hier gezeigte Beispiel stellt nur deine angegebenen Werte zur Hintergrundfarbe, Button-Farbe und die jeweilige Textfarbe dar. Um die anderen Effekte zu sehen, probier einfach mal ein bisschen rum und schau es dir im Front-End an :-)</p>';
-echo '</br>
-		<div id="cookiedingsbums_wrapper">
-			<div class="cookiedingsbums_content" '.$main_color_scheme.'>
-			<p class="cookiedingsbums_text"> This website uses cookies to ensure you get the best experience on our website <a class="cookiedingsbums_link" '.$link_color_scheme.' href="'.$link.'">'.$link_content.'</a> </p> 
-			<form class="cookiedingsbums_form">
-			<button class="cookiedingsbums_button" '.$button_color_scheme.'>Bestätige</button>
-			</form>
-			</div>
-	</div></br>';
-echo '<p>Um den Cookie Hinweis im Frontend darzustellen, nutze bitte die Funktion: <code><pre>echo cookie_consent::cookie_consent_output();</code></pre></p>';
-echo '<p>Alternative muss der folgende Code noch vor deinem schließenden head-Tag in einen script-Block gesetzt werden. Bitte nimm dir Zeit und lies dir vorher die Hilfe durch, damit auch alles reibungslos klappt</p>';
-echo cookie_consent::cookie_consent_backend();
+// ----------------- PREVIEW SECTION
+
+$notice = rex_view::info($this->i18n('test_preview_notice'));
+$fragment = new rex_fragment();
+$fragment->setVar('title', $this->i18n('preview_section'), true);
+$fragment->setVar('body', $notice.$previewSection, false);
+echo $fragment->parse('core/page/section.php');
+
+// ----------------- EMBED SECTION
+
+$frontendPathProvider = new rex_path_default_provider($REX['HTDOCS_PATH'], $REX['BACKEND_FOLDER'], false);
+$cssFileUrl = $frontendPathProvider->addonAssets($this->getName(), $cssFile);
+$jsFileUrl = $frontendPathProvider->addonAssets($this->getName(), $jsFile);
+
+$notice = rex_view::info($this->i18n('test_embed_notice'));
+$notice .= '<pre><code>'.$cssFileUrl.PHP_EOL.$jsFileUrl.'</code></pre>';
+$copyPasteCode = '<pre><code>'.htmlspecialchars(cookie_consent::cookie_consent_backend()).'</code></pre>';
+
+$fragment = new rex_fragment();
+$fragment->setVar('title', $this->i18n('config_code'), true);
+$fragment->setVar('body', $notice.$copyPasteCode, false);
+echo $fragment->parse('core/page/section.php');
