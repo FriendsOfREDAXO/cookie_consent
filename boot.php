8cd7471a525d8784f9040da96a35050f6907fbbd
Marcel Kuhmann <der_marcell@hotmail.com>
2019-03-10 17:24:00 +0100

    cs fix (#97)

--- boot.php
+++ boot.php
@@ -11,14 +11,14 @@ if (rex::isBackend() && rex_be_controller::getCurrentPage() == 'cookie_consent/c
     rex_view::addJsFile($this->getAssetsUrl('js/cookie_consent_backend.js'));
 }
 
-if(!rex::isBackend()) {
+if (!rex::isBackend()) {
     rex_extension::register('PACKAGES_INCLUDED', function ($params) {
-		if (rex_config::get('cookie_consent', cookie_consent::getKeyPrefix().'embed_auto') == '1') {
-			rex_extension::register('OUTPUT_FILTER', 'cookie_consent::ep_call', rex_extension::LATE);
-		}
+        if (rex_config::get('cookie_consent', cookie_consent::getKeyPrefix().'embed_auto') == '1') {
+            rex_extension::register('OUTPUT_FILTER', 'cookie_consent::ep_call', rex_extension::LATE);
+        }
 
-		if (cookie_consent::getMode() === cookie_consent::MODE_OPT_IN && !is_object(rex::getUser())) {
-			rex_extension::register('OUTPUT_FILTER', 'cookie_consent::ep_optin', rex_extension::LATE);
-		}
-	});
+        if (cookie_consent::getMode() === cookie_consent::MODE_OPT_IN && !is_object(rex::getUser())) {
+            rex_extension::register('OUTPUT_FILTER', 'cookie_consent::ep_optin', rex_extension::LATE);
+        }
+    });
 }
